0b1ddd2a11f3a6dd693200762c2d517a693605ed
alesolenne <alesolenne@gmail.com>
2025-05-13 20:34:14 +0200

    a

--- bringup/bringup_darko_manipuationl.sh
+++ bringup/bringup_darko_manipuationl.sh
@@ -13,19 +13,19 @@ if ! tmux has-session -t $SESSION_1 2>/dev/null; then
   tmux new-session -d -s $SESSION_1 -n "bring1" \
     "bash -c 'source $ROS_SETUP && source $WORKSPACE_SETUP && roslaunch darko_launch_system manipulation_complete.launch'"
 
-  sleep(5.0)
+  sleep 5.0
 
   # Crea la finestra manip2
   tmux new-window -t $SESSION_1:1 -n "bring2" \
     "bash -c 'source $ROS_SETUP && source $WORKSPACE_SETUP && roslaunch darko_robot_arm_control Moveitinterface.launch'"
 
-  sleep(10.0)
+  sleep 10.0
 
   # Crea la finestra manip3
   tmux new-window -t $SESSION_1:2 -n "bring3" \
     "bash -c 'source $ROS_SETUP && source $WORKSPACE_SETUP && roslaunch darko_launch_system rviz_stefano.launch'"
 
-  sleep(2.0)
+  sleep 2.0
 
   # Seleziona la finestra 'manip1'
   tmux select-window -t $SESSION_1:0
@@ -37,7 +37,7 @@ if ! tmux has-session -t $SESSION_2 2>/dev/null; then
   tmux new-session -d -s $SESSION_2 -n "ctrl1" \
     "bash -c 'source $ROS_SETUP && source $WORKSPACE_SETUP && roslaunch panda_wrist_softhand_control launchControlServer.launch'"
 
-  sleep(4.0)
+  sleep 4.0
 
   # Crea la finestra ctrl2
   tmux new-window -t $SESSION_2:1 -n "ctrl2" \
---
+++ bringup/tmux.sh
@@ -0,0 +1,29 @@
+# Aggiungi una scorciatoia per "uscire" (per chiudere la sessione tmux)
+bind -n C-q kill-session  # Ctrl+q per uccidere la sessione corrente
+bind -n C-F12 confirm-before -p "Chiudere tutte le sessioni tmux? (y/n)" "run-shell 'tmux kill-server'"
+# Passaggio rapido tra sessioni
+bind -n F1 switch-client -t camera
+bind -n F2 switch-client -t object_detection
+bind -n F3 switch-client -t tool_detection
+
+# Mostra messaggio sulla barra per conferma
+# Passaggio rapido tra sessioni con messaggio di conferma
+bind -n F1 run-shell "tmux switch-client -t camera; tmux display-message 'Switched to session: camera'"
+bind -n F2 run-shell "tmux switch-client -t object_detection; tmux display-message 'Switched to session: object_detection'"
+bind -n F3 run-shell "tmux switch-client -t tool_detection; tmux display-message 'Switched to session: tool_detection'"
+
+# Mostra i comandi nella status bar
+# Impostare i colori della barra di stato
+set -g status-style bg='#2e3440',fg='#d8dee9'
+
+# Barra di stato sinistra con scorciatoie e messaggi
+set -g status-left '#[fg=yellow] F1:camera #[fg=green] F2:obj_det #[fg=magenta] F3:tool_det #[fg=red] Ctrl+Q:quit #[fg=red] Ctrl+F12:exit_all            '
+
+# Impostare la lunghezza della barra di stato
+set -g status-left-length 90
+
+# Mouse
+set -g mouse on
+
+# Altezza
+set -g status-height 3
